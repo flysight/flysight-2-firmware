a375b3a46b491e1f5700d8aae7bbd8228c8b56bd
Michael Cooper <michael@flysight.ca>
2023-04-12 18:51:02 -0600

    Change sensor bus to 100 kHz

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -396,7 +396,7 @@ static void MX_I2C3_Init(void)
 
   /* USER CODE END I2C3_Init 1 */
   hi2c3.Instance = I2C3;
-  hi2c3.Init.Timing = 0x00300F38;
+  hi2c3.Init.Timing = 0x00707CBB;
   hi2c3.Init.OwnAddress1 = 0;
   hi2c3.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c3.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
