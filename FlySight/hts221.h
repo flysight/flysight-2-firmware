05c06711419c48a80fc14295bea890798213a1b3
Michael Cooper <michael@flysight.ca>
2024-01-06 15:11:56 -0700

    Make humidity sensor interface more flexible

---
+++ FlySight/hts221.h
@@ -0,0 +1,34 @@
+/***************************************************************************
+**                                                                        **
+**  FlySight 2 firmware                                                   **
+**  Copyright 2023 Bionic Avionics Inc.                                   **
+**                                                                        **
+**  This program is free software: you can redistribute it and/or modify  **
+**  it under the terms of the GNU General Public License as published by  **
+**  the Free Software Foundation, either version 3 of the License, or     **
+**  (at your option) any later version.                                   **
+**                                                                        **
+**  This program is distributed in the hope that it will be useful,       **
+**  but WITHOUT ANY WARRANTY; without even the implied warranty of        **
+**  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         **
+**  GNU General Public License for more details.                          **
+**                                                                        **
+**  You should have received a copy of the GNU General Public License     **
+**  along with this program.  If not, see <http://www.gnu.org/licenses/>. **
+**                                                                        **
+****************************************************************************
+**  Contact: Bionic Avionics Inc.                                         **
+**  Website: http://flysight.ca/                                          **
+****************************************************************************/
+
+#ifndef HTS221_H_
+#define HTS221_H_
+
+#include "hum.h"
+
+FS_Hum_Result_t FS_HTS221_Init(void);
+void FS_HTS221_Start(void);
+void FS_HTS221_Stop(void);
+void FS_HTS221_Read(FS_Hum_Data_t *data);
+
+#endif /* HTS221_H_ */
