3ce0a3a51c65b46c9b51af0832f5167157fcce9f
Michael Cooper <crwper@gmail.com>
2024-05-05 16:18:14 -0600

    Move random byte generation to common function

--- FlySight/common.c
+++ FlySight/common.c
@@ -24,6 +24,10 @@
 #include "main.h"
 #include "app_common.h"
 
+#define TIMEOUT_VALUE 100
+
+extern RNG_HandleTypeDef hrng;
+
 char *writeInt32ToBuf(char *ptr, int32_t val, int8_t dec, int8_t dot, char delimiter)
 {
     int32_t value = val > 0 ? val : -val;
@@ -49,4 +53,45 @@ char *writeInt32ToBuf(char *ptr, int32_t val, int8_t dec, int8_t dot, char delim
     }
 
 	return ptr;
+}
+
+void FS_Common_GetRandomBytes(uint32_t *buf, uint32_t count)
+{
+	HAL_StatusTypeDef res;
+	uint32_t counter;
+	uint32_t tickstart;
+
+	/* Algorithm to use RNG on CPU1 comes from AN5289 Figure 8 */
+
+	/* Poll Sem0 until granted */
+	LL_HSEM_1StepLock(HSEM, CFG_HW_RNG_SEMID);
+
+	/* Configure and switch on RNG clock*/
+	MX_RNG_Init();
+
+	/* Generate random session ID */
+	for (counter = 0; counter < count; ++counter)
+	{
+	    tickstart = HAL_GetTick();
+
+	    res = HAL_ERROR;
+		while ((res != HAL_OK) && (HAL_GetTick() - tickstart < TIMEOUT_VALUE))
+		{
+			res = HAL_RNG_GenerateRandomNumber(&hrng, &buf[counter]);
+		}
+
+		if (res != HAL_OK)
+		{
+			Error_Handler();
+		}
+	}
+
+	/* Switch off RNG IP and clock */
+	HAL_RNG_DeInit(&hrng);
+
+	/* Set RNGSEL = CLK48 */
+    LL_RCC_SetRNGClockSource(RCC_RNGCLKSOURCE_CLK48);
+
+	/* Release Sem0 */
+	LL_HSEM_ReleaseLock(HSEM, CFG_HW_RNG_SEMID, 0);
 }
