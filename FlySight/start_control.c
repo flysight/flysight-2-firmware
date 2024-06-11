4bb8da19fd85511de40c096030b1d8ce5dd28b69
Michael Cooper <michael@flysight.ca>
2024-06-11 15:59:22 -0600

    Fix resource leak in start mode

--- FlySight/start_control.c
+++ FlySight/start_control.c
@@ -112,6 +112,7 @@ void FS_StartControl_DeInit(void)
 
 	// Delete timer
 	HW_TS_Delete(led_timer_id);
+	HW_TS_Delete(count_timer_id);
 
 	// Disable charging
 	FS_Charge_SetCurrent(FS_CHARGE_DISABLE);
