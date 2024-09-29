8366b418eb43688770c9cde78fecc3cef3b5d962
Michael Cooper <crwper@gmail.com>
2024-09-29 13:39:00 -0600

    Add pairing request flag to manufacturer specific data

--- STM32_WPAN/App/app_ble.c
+++ STM32_WPAN/App/app_ble.c
@@ -1366,6 +1366,9 @@ static void APP_BLE_UpdateAdvertisingData(APP_BLE_ConnStatus_t NewStatus)
 
   if ((NewStatus == APP_BLE_FAST_ADV) || (NewStatus == APP_BLE_LP_ADV))
   {
+    /* Set flag in manufacturer specific data */
+    mfg_data[4] = request_pairing;
+
     /* Copy manufacturer specific data */
     memcpy(&(adv_data[k]), mfg_data, sizeof(mfg_data));
     k += sizeof(mfg_data);
