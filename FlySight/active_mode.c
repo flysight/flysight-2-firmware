edbc78a074e1988152b950f1e478660e20930e20
Michael Cooper <crwper@gmail.com>
2025-02-15 10:39:34 -0700

    Connect to Activelook glasses

--- FlySight/active_mode.c
+++ FlySight/active_mode.c
@@ -48,7 +48,7 @@ void FS_ActiveMode_Init(void)
 	uint8_t enable_flags;
 
 	/* Start scanning for BLE peripherals */
-    UTIL_SEQ_SetTask(1 << CFG_TASK_START_SCAN_ID, CFG_SCH_PRIO_0);
+	UTIL_SEQ_SetTask(1 << CFG_TASK_START_SCAN_ID, CFG_SCH_PRIO_0);
 
 	/* Initialize FatFS */
 	FS_ResourceManager_RequestResource(FS_RESOURCE_FATFS);
