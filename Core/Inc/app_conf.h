745a5f94807d97a39ce8ef8193fcc86a7a2f7159
Michael Cooper <crwper@gmail.com>
2025-02-17 09:22:36 -0700

    Add task definition

--- Core/Inc/app_conf.h
+++ Core/Inc/app_conf.h
@@ -706,6 +706,7 @@ typedef enum
     CFG_TASK_FS_AUDIO_CONTROL_PRODUCER_ID,
     CFG_TASK_FS_AUDIO_CONTROL_CONSUMER_ID,
     CFG_TASK_FS_CONFIG_UPDATE_ID,
+    CFG_TASK_FS_ACTIVELOOK_ID,
   /* USER CODE END CFG_Task_Id_With_NO_HCI_Cmd_t */
   CFG_LAST_TASK_ID_WITH_NO_HCICMD                                            /**< Shall be LAST in the list */
 } CFG_Task_Id_With_NO_HCI_Cmd_t;
