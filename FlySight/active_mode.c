02d482c2542d23b93752714447dcb2ffd7976739
Michael Cooper <crwper@gmail.com>
2025-05-17 12:23:56 -0600

    Fix issue with rebase

--- FlySight/active_mode.c
+++ FlySight/active_mode.c
@@ -38,7 +38,6 @@
 #include "resource_manager.h"
 #include "sensor.h"
 #include "state.h"
-#include "stm32_seq.h"
 #include "vbat.h"
 
 extern UART_HandleTypeDef huart1;
@@ -48,9 +47,6 @@ void FS_ActiveMode_Init(void)
 {
 	uint8_t enable_flags;
 	bool isSystemHealthy = true;
-
-	/* Start scanning for BLE peripherals */
-	UTIL_SEQ_SetTask(1 << CFG_TASK_START_SCAN_ID, CFG_SCH_PRIO_0);
 
 	/* Initialize FatFS */
 	FS_ResourceManager_RequestResource(FS_RESOURCE_FATFS);
